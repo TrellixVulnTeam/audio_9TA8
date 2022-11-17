424e6762eb007347c613d4c0ad335f103e12f686
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 07:31:05 +0000

    Adding tarfile member sanitization to extractall()

--- torchaudio/data.py
+++ torchaudio/data.py
@@ -68,7 +68,26 @@ class TarDataset(object):
                     tfile.write(tdata.read())
             print('Extracting {cls.filename} to {cls.dirname}'.format(cls=cls))
             with tarfile.open(tpath, 'r') as tfile:
-                tfile.extractall(path=root)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tfile, path=root)
             print('Removing {cls.filename}'.format(cls=cls))
             os.unlink(tpath)
         return os.path.join(path, '')
